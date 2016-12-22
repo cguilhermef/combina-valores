6344f0d8a050340b5c53c4d589a960737840ab09
Christian Guilherme Fritsch <guilherme@opiniaoearte.com.br>
2016-12-21 23:11:12 -0200

    modificadao  o metodo de execução para o novo algoritmo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,27 +10,7 @@
 angular.module('chequesApp')
   .controller('MainCtrl', function ($scope) {
     $scope.init = function() {
-      $scope.cheques = [
-        142.75300732942,
-        1992.2281988627,
-        762.59871167717,
-        2672.7218359815,
-        593.75581408094,
-        2016.807615695,
-        1830.2684736346,
-        928.29185208692,
-        1562.3968399653,
-        1778.9732755995,
-        77.315295826325,
-        511.72777379943,
-        1391.2501540926,
-        2153.8754689758,
-        2646.5870890052,
-        1866.98667175,
-        95.096584733155,
-        2962.5126132334,
-        1852.2217330766,
-        2001.5792618047];
+      $scope.cheques = [687.34, 648, 440, 408.58, 200, 581];
       $scope.model = {
         cheque: null
       };
@@ -45,13 +25,20 @@ angular.module('chequesApp')
     };
 
     $scope.clearResults = function() {
-      $scope.troco = 0;
-      $scope.melhorCombinacaoComTroco = [];
-      $scope.menorDiferenca = 0;
-      $scope.diferencaMaiorCombinacao = 0;
-      $scope.maiorCombinacaoComDiferenca = [];
-      $scope.menorCombinacaoComDiferenca = [];
-      $scope.melhorCombinacaoComDiferenca = [];
+      $scope.menorTroco = _.sum($scope.valores) - $scope.valor;
+      $scope.menorTrocoComMaisCheques = _.sum($scope.valores) - $scope.valor;
+      $scope.menorTrocoComMenosCheques = _.sum($scope.valores) - $scope.valor;
+
+      $scope.menorDiferenca = $scope.valor;
+      $scope.menorDiferencaMaisCheques = $scope.valor;
+      $scope.menorDiferencaMenosCheques = $scope.valor;
+
+      $scope.combinacaoMenorTroco = [];
+      $scope.combinacaoMenorTrocoMaisCheques = [];
+      $scope.combinacaoMenorTrocoMenosCheques = [];
+      $scope.combinacaoMenorDiferenca = [];
+      $scope.combinacaoMenorDiferencaMaisCheques = [];
+      $scope.combinacaoMenorDiferencaMenosCheques = [];
     };
 
     $scope.addCheque = function() {
@@ -65,58 +52,52 @@ angular.module('chequesApp')
 
     $scope.execute = function() {
       $scope.clearResults();
-      var valor = $scope.valor;
-      var i = 0;
-      var j = 0;
-      var ln = $scope.cheques.length;
-      var asc = _.sortBy($scope.cheques);
-      var matriz = [];
-      var valorA = 0;
-      var valorB = _.sum(asc)-valor;
-      var somaTmp = 0;
-      var combinacaoBaixo = null;
-      var combinacaoAcima = null;
-      var menorNumeroChequesComDiferenca = _.times(ln, _.constant(0));
-      var maiorNumeroChequesComDiferenca = [];
-      for(i = 0; i < ln; i++ ) {
-        matriz.push([]);
-        for(j = 0; j < ln; j++) {
-          if ( asc[(i+j)%ln] > 0) {
+      var ln = $scope.valores.length;
+      var cp = Math.pow(2, ln);
+      var tmpVetor = [];
+      var tmpValoresCombinados = [];
+      var tmpNum = null;
+      var i = 1;
+      var c = 0;
+      var valor = 0;
+      var tmpSoma = 0;
+      var menorDiferenca = valor;
 
-            matriz[i][j] = asc[(i+j)%ln];
-            somaTmp = _.sum(matriz[i]);
-            if (somaTmp <= valor && somaTmp > valorA) {
-              valorA = somaTmp;
-              combinacaoBaixo = [].concat(matriz[i]);
-              /**
-               * o menor numero esta bugando, não apresenta o melhor resultado.
-               */
-              if (matriz[i].length < menorNumeroChequesComDiferenca.length) {
-                menorNumeroChequesComDiferenca = [].concat(matriz[i]);
-              }
-              if (matriz[i].length > maiorNumeroChequesComDiferenca.length) {
-                maiorNumeroChequesComDiferenca = [].concat(matriz[i]);
-              }
+      while (i < cp) {
+        tmpValoresCombinados = [];
+        tmpVetor = _.map(_.padStart(Number(i).toString(2), ln, '0').split(''), Number);
+        
+        for(c = 0; c < ln; c++ ){
+          if (tmpVetor[c]) {
+            tmpValoresCombinados.push(valores[c]);
+          }
+        }
+        tmpSoma = _.sum(tmpValoresCombinados);
+        if (tmpSoma <= valor ){
+          if (valor - tmpSoma <= menorDiferenca) {
+            menorDiferenca = valor - tmpSoma;
+            combinacaoMenorDiferenca = [].concat(tmpValoresCombinados);
+          }
+        } else {
+          if (tmpSoma - valor < menorTroco) {
+            menorTroco = tmpSoma-valor;
+            combinacaoMenorTroco = [].concat(tmpValoresCombinados);
+            if (tmpValoresCombinados.length < combinacaoMenorDiferencaMenosCheques.length) {
+              menorTrocoComMenosCheques = tmpSoma-valor;
+              combinacaoMenorTrocoMenosCheques = [].concat(tmpValoresCombinados);
             }
-            if (somaTmp > valor && ((somaTmp - valor) > 0) && somaTmp - valor < valorB && (somaTmp - valor < valor)) {
-              valorB = somaTmp-valor;
-              combinacaoAcima = [].concat(matriz[i]);
+            if (tmpValoresCombinados.length > combinacaoMenorDiferencaMaisCheques.length) {
+              menorTrocoComMaisCheques = tmpSoma-valor;
+              combinacaoMenorTrocoMaisCheques = [].concat(tmpValoresCombinados);
             }
           }
         }
+        i++;
       }
-      $scope.melhorCombinacaoComTroco = combinacaoAcima;
-      $scope.troco = valorB;
-      $scope.melhorCombinacaoComDiferenca = combinacaoBaixo;
-      $scope.menorDiferenca = valor - valorA;
-      $scope.maiorCombinacaoComDiferenca = maiorNumeroChequesComDiferenca;
-      $scope.diferencaMaiorCombinacao = valor - _.sum(maiorNumeroChequesComDiferenca);
-      $scope.menorCombinacaoComDiferenca = menorNumeroChequesComDiferenca;
-      $scope.diferencaMenorCombinacao = valor - _.sum(menorNumeroChequesComDiferenca);
-      // console.log('com menos cheques:', valorA, menorNumeroChequesComDiferenca);
-      // console.log('com mais cheques:', valorA, maiorNumeroChequesComDiferenca);
-      // console.log('Melhor combinação sem troco:', valorA, combinacaoBaixo);
-      // console.log('Menor troco:', valorB, 'usando o(s) cheque(s):',combinacaoAcima);
+
+      console.log('Menor troco: ' + menorTroco, combinacaoMenorTroco);
+      console.log('Menor diferença: ' + menorDiferenca, combinacaoMenorDiferenca);
+
     }
 
     $scope.init();
