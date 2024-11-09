b702f711aff7cef5722a290a0fa1b0768ad93af3
Christian G. Fritsch <christiangfritsch@gmail.com>
2024-11-09 18:50:09 -0300

    build: corrige gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,96 @@
-var gulp          = require('gulp');
-var notify        = require('gulp-notify');
-var source        = require('vinyl-source-stream');
-var browserify    = require('browserify');
-var babelify      = require('babelify');
-var ngAnnotate    = require('browserify-ngannotate');
-var browserSync   = require('browser-sync').create();
-var rename        = require('gulp-rename');
-var templateCache = require('gulp-angular-templatecache');
-var uglify        = require('gulp-uglify');
-var merge         = require('merge-stream');
+var gulp = require("gulp");
+var notify = require("gulp-notify");
+var source = require("vinyl-source-stream");
+var browserify = require("browserify");
+var babelify = require("babelify");
+var ngAnnotate = require("browserify-ngannotate");
+var browserSync = require("browser-sync").create();
+var rename = require("gulp-rename");
+var templateCache = require("gulp-angular-templatecache");
+var uglify = require("gulp-uglify");
+var merge = require("merge-stream");
 
 // Where our files are located
-var jsFiles   = "app/scripts/**/*.js";
+var jsFiles = "app/scripts/**/*.js";
 var viewFiles = "app/**/*.html";
 
-var interceptErrors = function(error) {
+var interceptErrors = function (error) {
   var args = Array.prototype.slice.call(arguments);
 
   // Send error to notification center with gulp-notify
   // @ts-ignore
-  notify.onError({
-    title: 'Compile Error',
-    message: '<%= error.message %>'
-  }).apply(this, args);
+  notify
+    .onError({
+      title: "Compile Error",
+      message: "<%= error.message %>",
+    })
+    .apply(this, args);
 
   // Keep gulp from hanging on this task
-  this.emit('end');
+  this.emit("end");
 };
 
-
-gulp.task('browserify', ['views'], function() {
-  return browserify([
-    './src/scripts/app.js',
-    './src/scripts/controllers/main.js',
-    './src/scripts/directives/on-enter.js'
-  ])
-      .transform(babelify, {presets: ["es2015"]})
+gulp.task("browserify", ["views"], function () {
+  return (
+    browserify([
+      "./src/scripts/app.js",
+      "./src/scripts/controllers/main.js",
+      "./src/scripts/directives/on-enter.js",
+    ])
+      .transform(babelify, { presets: ["es2015"] })
       .transform(ngAnnotate)
       .bundle()
-      .on('error', interceptErrors)
+      .on("error", interceptErrors)
       //Pass desired output filename to vinyl-source-stream
-      .pipe(source('main.js'))
+      .pipe(source("main.js"))
       // Start piping stream to tasks!
-      .pipe(gulp.dest('./build/'));
+      .pipe(gulp.dest("./dist/"))
+  );
 });
 
-gulp.task('html', function() {
-  return gulp.src([
-    "src/index.html",
-    "src/views/main.html"
-  ])
-      .on('error', interceptErrors)
-      .pipe(gulp.dest('./build/'));
+gulp.task("html", function () {
+  return gulp
+    .src(["src/index.html", "src/views/main.html"])
+    .on("error", interceptErrors)
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('views', function() {
-  return gulp.src(viewFiles)
-      .pipe(templateCache({
-        standalone: true
-      }))
-      .on('error', interceptErrors)
-      .pipe(rename("app.templates.js"))
-      .pipe(gulp.dest('./src/js/config/'));
+gulp.task("views", function () {
+  return gulp
+    .src(viewFiles)
+    .pipe(
+      templateCache({
+        standalone: true,
+      })
+    )
+    .on("error", interceptErrors)
+    .pipe(rename("app.templates.js"))
+    .pipe(gulp.dest("./src/js/config/"));
 });
 
 // This task is used for building production ready
 // minified JS/CSS files into the dist/ folder
-gulp.task('build', ['html', 'browserify'], function() {
-  var html = gulp.src([
-    "build/index.html",
-    "build/main.html"
-  ])
-                 .pipe(gulp.dest('./dist/'));
+gulp.task("build", ["html", "browserify"], function () {
+  var html = gulp
+    .src(["build/index.html", "build/main.html"])
+    .pipe(gulp.dest("./dist/"));
 
-  var js = gulp.src("build/main.js")
-               .pipe(uglify())
-               .pipe(gulp.dest('./dist/'));
+  var js = gulp.src("build/main.js").pipe(uglify()).pipe(gulp.dest("./dist/"));
 
-  return merge(html,js);
+  return merge(html, js);
 });
 
-gulp.task('default', ['html', 'browserify'], function() {
-
-  browserSync.init(['./build/**/**.**'], {
+gulp.task("default", ["html", "browserify"], function () {
+  browserSync.init(["./dist/**/**.**"], {
     server: "./build",
     port: 4000,
     notify: false,
     ui: {
-      port: 4001
-    }
+      port: 4001,
+    },
   });
 
-  gulp.watch("src/index.html", ['html']);
-  gulp.watch(viewFiles, ['views']);
-  gulp.watch(jsFiles, ['browserify']);
+  gulp.watch("src/index.html", ["html"]);
+  gulp.watch(viewFiles, ["views"]);
+  gulp.watch(jsFiles, ["browserify"]);
 });
